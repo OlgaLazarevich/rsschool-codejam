8ed18dfc853b4f650e76a6c7cd29f98d1cce30a7
helga <helga>
2018-10-28 18:01:39 +0300

    refactor: for function make

--- task/make.js
+++ task/make.js
@@ -1,12 +1,16 @@
-module.exports = function make(first) {
-  let firstArg = [...arguments];
+module.exports = function make(a) {
+  let newArr = [a];
+  function f(b) {
+    if (typeof (b) === 'function') {
+      return newArr.reduce(b);
+    }
+    let args = [...arguments];
+    args.forEach((item, i, arr) => newArr.push(args[i]));
+    return f;
+  }
+  return f;
+};
 
-  return function (second) {
-    const secondArg = [...arguments];
-    const allArgs = [...firstArg, ...secondArg];
-    return (fun) => allArgs.reduce(fun);
-  };
+function sum(a, b) {
+  return a + b;
 }
-// function sum(a, b) {
-//   return a + b;
-// }
